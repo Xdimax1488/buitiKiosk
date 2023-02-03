e2a495960c08d5a8f0b04d4603e9d4aa50400968
Dima <dimakurinnoy8@gmail.com>
2023-02-03 23:32:50 +0100

    fix reduser

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,12 +1,14 @@
-import { applyMiddleware, combineReducers, createStore} from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { productListReducer } from './Reducers/ProductReducer';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList:productListReducer
+});
 
 const initialState = {
-    productList:productListReducer
+    
 };
 
 const middleware = [thunk];
